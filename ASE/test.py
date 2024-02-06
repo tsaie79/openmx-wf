d2869220b272b5ac8220171d69fecce6491adb2f
tsaie79 <tsaie79@gmail.com>
2024-02-06 22:29:22 +0000

    Add OpenMX calculation setup for GaAs structure

--- ASE/test.py
+++ ASE/test.py
@@ -8,19 +8,45 @@ import os
 
 os.chdir(os.path.dirname(__file__))
 
-# add environment variables, export OPENMX_DFT_DATA_PATH=/openmx/DFT_DATA13
-os.environ["OPENMX_DFT_DATA_PATH"] = "/workspaces/openmx-wf/ASE/DFT_DATA19"
-os.environ["ASE_OPENMX_COMMAND"] = "openmx"
+class CalculationSetup:
+    def __init__(self, input_file, magnetic_moments=None):
+        self.input_file = input_file
 
+        # load structure from file
+        self.st = Structure.from_file(self.input_file)
 
-st = Structure.from_file("C.cif")
+        atom_adaptor = AseAtomsAdaptor()
+        self.atoms = atom_adaptor.get_atoms(self.st)
 
-atom_adaptor = AseAtomsAdaptor()
+        # set magnetic moments
+        if magnetic_moments:
+            self.atoms.set_initial_magnetic_moments(magnetic_moments)
 
-atoms = atom_adaptor.get_atoms(st)
+    def setup_calculation(self):
+        # add environment variables
+        os.environ["OPENMX_DFT_DATA_PATH"] = "/workspaces/openmx-wf/ASE/DFT_DATA19"
+        os.environ["ASE_OPENMX_COMMAND"] = "openmx"
 
-calc = OpenMX(xc="PBE")
+        # a set of configuration as a dictionary
+        inputs = dict(
+            scf_xctype="GGA-PBE",
+            scf_kgrid=(4, 4, 4),
+            scf_maxiter=40,
+            scf_mixing_type="Simple",
+            scf_spinpolarization="off",
+            scf_energycutoff=200.0,
+            scf_eigenvaluesolver="Band",
+            md_type="nomd",
+        )
 
-calc.write_input(atoms)
+        calc = OpenMX(label=f"{self.st.formula}_openmx", **inputs)
+        calc.write_input(self.atoms, properties=["magmoms", "forces", "stress"])
 
+def main():
+    input_file = "GaAs.vasp"
+    calc_setup = CalculationSetup(input_file, magnetic_moments=[2, 2, -1, -1, 0, 0, 0, 0])
+    print(calc_setup.atoms.get_initial_magnetic_moments())
+    calc_setup.setup_calculation()
 
+if __name__ == "__main__":
+    main()
