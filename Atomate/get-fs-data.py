8aa4a80ae367dac11e1cd41642fd62c5d6810b86
tsaie79 <tsaie79@gmail.com>
2024-02-14 14:26:09 +0000

    Add functions to retrieve and save OpenMX data

--- Atomate/get-fs-data.py
+++ Atomate/get-fs-data.py
@@ -1,16 +1,45 @@
 from atomate.openmx.database import openmxCalcDb
+import os
+
+collections = [
+    "element_dat",
+    "hamiltonians_h5",
+    "lat_dat",
+    "orbital_types_dat",
+    "rc_h5",
+    "rh_h5",
+    "rlat_dat",
+    "R_list_dat",
+    "site_positions_dat",
+]
+
+OBJ_NAMES = tuple(coll for coll in collections)
+
+OPENMX_DB = openmxCalcDb.from_db_file("/workspaces/openmx-wf/Atomate/setting/db.json", admin=True)
 
 
-DB_PATH = "/workspaces/openmx-wf/Atomate/setting/db.json"
+def get_fs_file(taskid, key, f_name, db=OPENMX_DB):
+    out = db.get_openmx_output(taskid, key)
+    with open(f"/workspaces/openmx-wf/{f_name}", "wb") as f:
+        f.write(out)
 
-# def get_fs_data():
-#     db = openmxCalcDb.from_db_file(DB_PATH, admin=True)
-#     fs_d
-#     return fs_data
+def get_deeph_files(taskid, db=OPENMX_DB, objs=OBJ_NAMES):
+    dir_path = "/workspaces/openmx-wf/deeph"
+    os.mkdir(dir_path)
+    # run the following loop in parallel
+    import concurrent.futures
+
+    def write_output(obj):
+        out = db.get_openmx_output(taskid, obj)
+        # define the file name by replace the last "_" with "." of obj
+        obj = obj[::-1].replace("_", ".", 1)[::-1]
+        with open(f"{dir_path}/{obj}", "wb") as f:
+            f.write(out)
+
+    # Use a ThreadPoolExecutor to run the loop in parallel
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(write_output, objs)
+
 
 if __name__ == "__main__":
-    db = openmxCalcDb.from_db_file(DB_PATH, admin=True)
-    out = db.get_openmx_scfout(1)
-
-    with open("/workspaces/openmx-wf/openmx.scfout", "wb") as f:
-        f.write(out)
+    get_deeph_files(1)
