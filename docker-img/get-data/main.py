463a508959ddf3b97d8adbf4e3d03db9aa23185e
tsaie79 <tsaie79@gmail.com>
2024-03-18 03:12:47 +0000

    Refactor get_files function to retrieve openmx and deephe3 files from the database

--- docker-img/get-data/main.py
+++ docker-img/get-data/main.py
@@ -1,74 +1,55 @@
+#!/usr/bin/env python3
+
 from atomate.openmx.database import openmxCalcDb
 import argparse
+import concurrent.futures
+import os
 
-collections = [
-    "element_dat",
-    "hamiltonians_h5",
-    "lat_dat",
-    "orbital_types_dat",
-    "rc_h5",
-    "rh_h5",
-    "rlat_dat",
-    "R_list_dat",
-    "site_positions_dat",
-]
-
-OBJ_NAMES = tuple(coll for coll in collections)
-
-
-
-def get_fs_file(taskid, key, f_name, db="db.json"):
-    """
-    Get the file from the database and write it to the file system
-    Args:
-        taskid (int): The task id of the calculation
-        key (str): The key of the file to get
-        f_name (str): The name of the file to write to
-        db (openmxCalcDb): The database to get the file from
-    """
+def get_files(task_id, store_in, db="db.json", path="openmx_raw"):
     db = openmxCalcDb.from_db_file(db, admin=True)
-    out = db.get_openmx_output(taskid, key)
-    with open(f"{f_name}", "wb") as f:
-        f.write(out)
+    keys = db.collection.find_one({"task_id": task_id})["calcs_reversed"][0][path].keys()
+    keys = [key.split("_fs_id")[0] for key in keys if "_fs_id" in key]
+    print(f"Keys: {keys}")
+    def write_output(key):
+        out = db.get_openmx_output(task_id, key, path)
+        # make the directory if it does not exist
+        print(f"---> {store_in}/{task_id}/{path}/{key} <---")
+        if not os.path.exists(f"{store_in}/{task_id}/{path}"):
+            os.makedirs(f"{store_in}/{task_id}/{path}")
+            print(f"Directory {store_in}/{task_id}/{path} created")
 
-def get_deeph_files(taskid, store_in, db="db.json", objs=OBJ_NAMES):
-    """
-    Get the files from the database and write them to the file system
-    Args:
-        taskid (int): The task id of the calculation
-        db (openmxCalcDb): The database to get the files from
-        objs (tuple): The names of the files to get
-    """
-    db = openmxCalcDb.from_db_file(db, admin=True)
 
-    # run the following loop in parallel
-    import concurrent.futures
-    def write_output(obj):
-        out = db.get_openmx_output(taskid, obj)
-        # define the file name by replace the last "_" with "." of obj
-        obj = obj[::-1].replace("_", ".", 1)[::-1]
-        with open(f"{store_in}/{obj}", "wb") as f:
+        if path == "openmx_rst":
+            key = key.replace("_", ".", 1)
+        else:
+        # replace the last "_" with a "." in key
+            key = key[::-1].replace("_", ".", 1)[::-1]
+
+        with open(f"{store_in}/{task_id}/{path}/{key}", "wb") as f:
             f.write(out)
+
 
     # Use a ThreadPoolExecutor to run the loop in parallel
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(write_output, objs)
+        executor.map(write_output, keys)
 
+    
 
 def main():
     # Create the parser
-    parser = argparse.ArgumentParser(description="Get deepH files.")
+    parser = argparse.ArgumentParser(description="Get the openmx and deephe3 files from the database")
 
     # Add the arguments
-    parser.add_argument('-d', '--db_file', type=str, help='The path to the database file')
-    parser.add_argument('-t', '--taskid', type=int, required=True, help='The task id of the calculation')
-    parser.add_argument('-s', '--store_in', type=str, required=True, help='The directory to store the files in')
+    parser.add_argument('-t', '--taskid', type=int, help='The task id of the calculation', required=True)
+    parser.add_argument('-s', '--store_in', type=str, help='The directory to store the files in', default="/data")
+    parser.add_argument('-d', '--db_file', type=str, help='The path to the database file', default="/deeph/db.json")
+    parser.add_argument('-p', '--path', type=str, help='3 options: deeph_raw, openmx_rst, openmx_raw', required=True)
 
     # Parse the arguments
     args = parser.parse_args()
 
-    # Call the function with the provided arguments
-    get_deeph_files(args.taskid, store_in=args.store_in, db=args.db_file)
+    get_files(args.taskid, args.store_in, args.db_file, args.path)
+
 
 if __name__ == "__main__":
     main()  
