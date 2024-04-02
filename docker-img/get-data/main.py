9ab71745d8fe38e61513379d6f20266637fae254
tsaie79 <tsaie79@gmail.com>
2024-04-02 16:49:37 +0000

    Add support for writing deeph file and use a lock for parallel file writing

--- docker-img/get-data/main.py
+++ docker-img/get-data/main.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python3
 
 from atomate.openmx.database import openmxCalcDb
+from monty.serialization import dumpfn
 import argparse
 import concurrent.futures
 import os
+import threading
 
 def get_files(task_id, store_in, db="db.json", path="openmx_raw"):
     db = openmxCalcDb.from_db_file(db, admin=True)
-    keys = db.collection.find_one({"task_id": task_id})["calcs_reversed"][0][path].keys()
+    entry = db.collection.find_one({"task_id": task_id})
+
+
+        
+    # write raw
+    keys = entry["calcs_reversed"][0][path].keys()
     keys = [key.split("_fs_id")[0] for key in keys if "_fs_id" in key]
     print(f"Keys: {keys}")
+    store_in = f"{store_in}/{path}/{task_id}"
+
     def write_output(key):
         out = db.get_openmx_output(task_id, key, path)
         # make the directory if it does not exist
-        print(f"---> {store_in}/{task_id}/{path}/{key} <---")
-        if not os.path.exists(f"{store_in}/{task_id}/{path}"):
-            os.makedirs(f"{store_in}/{task_id}/{path}")
-            print(f"Directory {store_in}/{task_id}/{path} created")
+        print(f"---> {store_in} <---")
+        if not os.path.exists(f"{store_in}"):
+            os.makedirs(f"{store_in}")
+            print(f"Directory {store_in} created")
 
+            # write the deeph file to the store_in directory f"{store_in}/{task_id}/{path}/info.json"
+            print(f"---> {store_in}/info.json <---")
+            deep_info = entry["deeph"]
+            print(deep_info)
+            dumpfn(deep_info, f"{store_in}/info.json")
 
         if path == "openmx_rst":
             key = key.replace("_", ".", 1)
@@ -25,13 +39,23 @@ def get_files(task_id, store_in, db="db.json", path="openmx_raw"):
         # replace the last "_" with a "." in key
             key = key[::-1].replace("_", ".", 1)[::-1]
 
-        with open(f"{store_in}/{task_id}/{path}/{key}", "wb") as f:
+        with open(f"{store_in}/{key}", "w") as f:
             f.write(out)
 
 
+
+    # Create a lock
+    lock = threading.Lock()
+
+    def write_output_with_lock(key):
+        # Acquire the lock
+        with lock:
+            # Call the original function
+            write_output(key)
+
     # Use a ThreadPoolExecutor to run the loop in parallel
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(write_output, keys)
+        executor.map(write_output_with_lock, keys)
 
     
 
