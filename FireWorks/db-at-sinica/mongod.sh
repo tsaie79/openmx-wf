22306b73297808ccb2754ba634cb63f3cb7fa3d7
tsaie79 <tsaie79@gmail.com>
2024-05-15 02:11:56 +0000

    Refactor code to use absolute path for MongoDB data directory

--- FireWorks/db-at-sinica/mongod.sh
+++ FireWorks/db-at-sinica/mongod.sh
@@ -4,7 +4,7 @@
 mkdir -p $DB_PATH/log/
 touch $DB_PATH/log/mongodb.log
 
-export DB_PATH="/shared/Project/NCKU/Deep-SK/datadb/MongoDB"
+export DB_PATH="/home/tsaie79/db/MongoDB" 
 export MONGODB_IMAGE=$HOME/mongo_latest.sif
 
 singularity exec -B $DB_PATH/data/db:/data/db -B $DB_PATH/log/:/var/log/ $MONGODB_IMAGE mongod --logpath /var/log/mongodb.log --dbpath /data/db --bind_ip_all &
